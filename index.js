9b532cfab141d898e1c1c84a2318898bac68aabf
Vincenzo M <vincenzomaiorana@gmail.com>
2023-01-17 17:31:31 +0100

    add hot function

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const collapse = (id) => {
    document.querySelector(id).classList.toggle("collapse")
 }
+
+const removeCards = () => {
+   let cards = document.querySelectorAll(".card")
+   for (let i = 0; i < cards.length; i++) {
+      cards[i].parentElement.removeChild(cards[i])
+   }
+}
+
+const addHot = () => {
+   let toBeHot = document.querySelectorAll("#summerContent .card")
+   let hotTag = `<span class="badge text-bg-danger position-absolute top-0 end-0 m-2">HOT</span>`
+   for (let i = 0; i < toBeHot.length; i++) {
+      toBeHot[i].innerHTML += hotTag
+   }
+}
